68e70fe2ed359bcc0beff763a29a168c0f1efed3
Juan Carlos Coronado Velasquez <juan.coronado@office365.3pillarglobal.com>
2021-09-08 14:54:14 -0700

    cambiar package por compile en sh

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -10,7 +10,7 @@ pipeline {
         }
         stage('Build') {
             steps {
-                sh './mvnw clean package'
+                sh './mvnw clean compile'
                 //sh 'false' // true
             }
         
