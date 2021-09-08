cecc6fefc6b29c8a3069c172f98bace3464fe41f
Juan Carlos Coronado Velasquez <juan.coronado@office365.3pillarglobal.com>
2021-09-08 15:18:55 -0700

    cambiar package por compile en sh y cambiar credenciales en settings

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/yiyo12/jgsu-spring-petclinic.git', branch: 'main'
+                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
                //git url: 'https://github.com/yiyo12/jgsu-spring-petclinic.git', branch: 'main'
             }            
         }
