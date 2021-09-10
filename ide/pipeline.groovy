a130a3787409c1956febdcaf63e0f3a3eaecfcb1
Juan Carlos Coronado Velasquez <juan.coronado@office365.3pillarglobal.com>
2021-09-10 15:59:00 -0700

    intento 2 de hacer funcionar mi pipe

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -4,13 +4,14 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
-               //git url: 'https://github.com/yiyo12/jgsu-spring-petclinic.git', branch: 'main'
+                //git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
+               git url: 'https://github.com/yiyo12/jgsu-spring-petclinic.git', branch: 'main'
             }            
         }
         stage('Build') {
             steps {
-                sh './mvnw clean compile'
+                bat 'mvn clean compile'
+                //sh './mvnw clean compile'
                 //sh 'false' // true
             }
         
